8a4f471534b337168ed5b81627c96de4d03f5d7a
kaliassistant <1325077@gmail.com>
2025-09-28 20:17:57 +0800

    Lichee-jack core utils

---
+++ jack-coreutils/autobuild.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+set -e
+
+rm -rf build pkg
+
+ppwd=$(pwd)
+
+echo "Build jack-coreutils..."
+mkdir -p ./build/usr/bin
+mkdir -p ./build/usr/lib/systemd/system
+cp -r ./DEBIAN ./build
+cp src/scripts/* ./build/usr/bin
+cp src/systemd/jack-coreutils.service ./build/usr/lib/systemd/system
+chmod +x ./build/usr/bin/*
+mkdir -p ./pkg
+
+dpkg-deb --build build ./pkg/jack-coreutils_0.0.1_licheejack_riscv64.deb

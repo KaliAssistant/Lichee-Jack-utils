5c8c4902d88e1a1259b83cf94f302d9b738b1255
kaliassistant <1325077@gmail.com>
2025-10-18 21:36:17 +0800

    a lot commit

--- jack-coreutils/autobuild.sh
+++ jack-coreutils/autobuild.sh
@@ -8,11 +8,17 @@ ppwd=$(pwd)
 echo "Build jack-coreutils..."
 mkdir -p ./build/usr/bin
 mkdir -p ./build/usr/lib/systemd/system
-mkdir -p ./build/root/config
+mkdir -p ./build/etc/gt/templates
+mkdir -p ./build/etc/NetworkManager/system-connections
+mkdir -p ./build/root/config/udhcpd
 cp -r ./DEBIAN ./build
 cp src/scripts/* ./build/usr/bin
-cp src/systemd/jack-coreutils.service ./build/usr/lib/systemd/system
+cp src/systemd/*.service ./build/usr/lib/systemd/system
+cp src/config/udhcpd/*.conf ./build/root/config/udhcpd
+cp src/etc/gt/templates/*.scheme ./build/etc/gt/templates
+cp src/etc/NetworkManager/system-connections/*.nmconnection ./build/etc/NetworkManager/system-connections
 chmod +x ./build/usr/bin/*
+chmod 600 ./build/etc/NetworkManager/system-connections/*.nmconnection
 mkdir -p ./pkg
 
 dpkg-deb --build build ./pkg/jack-coreutils_0.0.1_licheejack_riscv64.deb

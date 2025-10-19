a7c7611fb6ededc4ac75c8bb0def89e7bcc42d7c
kaliassistant <1325077@gmail.com>
2025-10-19 19:35:38 +0800

    add motd logo

--- jack-coreutils/autobuild.sh
+++ jack-coreutils/autobuild.sh
@@ -10,6 +10,7 @@ mkdir -p ./build/usr/bin
 mkdir -p ./build/usr/lib/systemd/system
 mkdir -p ./build/etc/gt/templates
 mkdir -p ./build/etc/NetworkManager/system-connections
+mkdir -p ./build/etc/update-motd.d
 mkdir -p ./build/root/config/udhcpd
 cp -r ./DEBIAN ./build
 cp src/scripts/* ./build/usr/bin
@@ -17,7 +18,10 @@ cp src/systemd/*.service ./build/usr/lib/systemd/system
 cp src/config/udhcpd/*.conf ./build/root/config/udhcpd
 cp src/etc/gt/templates/*.scheme ./build/etc/gt/templates
 cp src/etc/NetworkManager/system-connections/*.nmconnection ./build/etc/NetworkManager/system-connections
+cp src/etc/update-motd.d/* ./build/etc/update-motd.d
+
 chmod +x ./build/usr/bin/*
+chmod +x ./build/etc/update-motd.d/*
 chmod 600 ./build/etc/NetworkManager/system-connections/*.nmconnection
 mkdir -p ./pkg
 
